f003a38d106efaa154c5214c2896eef7a7a22186
msaaddev <mrsaadirfan@gmail.com>
2019-07-29 18:18:49 +0500

    �ðDONE: Vidly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import Movies from "./components/movies";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Movies />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
