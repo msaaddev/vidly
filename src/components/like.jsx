741a8f3ed6d6c215035ea33bf0554107c76723f5
msaaddev <mrsaadirfan@gmail.com>
2019-08-03 18:00:35 +0500

    👌IMPROVE: like button

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Like extends Component {
-    state = {  }
-    render() {
-        return (
-            <i class="fa fa-heart-o" aria-hidden="true"></i>
-         );
-    }
+  state = {};
+
+  clickEvent() {
+    return <i class="fa fa-heart" aria-hidden="true" />;
+  }
+  render() {
+    let classes = "fa fa-heart";
+    if (!this.props.liked) classes += "-o";
+    return (
+      <i
+        className={classes}
+        style={{ cursor: "pointer" }}
+        onClick = {this.props.onClick}
+        aria-hidden="true"
+      />
+    );
+  }
 }
 
 export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./like"
+import Like from "./like";
 
 class Movies extends Component {
   state = {
@@ -9,6 +9,14 @@ class Movies extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index]};
+    movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
 
@@ -37,7 +45,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
-                <td><Like/></td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
