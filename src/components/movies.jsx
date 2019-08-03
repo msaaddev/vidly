a225228f89d90204cd1e067b3ae76f6a25938a0e
msaaddev <mrsaadirfan@gmail.com>
2019-08-03 19:27:01 +0500

    📦NEW: pagination component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./like";
+import Pagination from "./pagination";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = movie => {
@@ -15,13 +18,18 @@ class Movies extends Component {
   handleLike = movie => {
     const movies = [...this.state.movies];
     const index = movies.indexOf(movie);
-    movies[index] = { ...movies[index]};
+    movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
 
+  handlePageChange = page => {
+    this.setState({currentPage: page})
+  }
+
   render() {
     const { length: count } = this.state.movies;
+    const {pageSize, currentPage} = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -63,6 +71,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
       </React.Fragment>
     );
   }
---
+++ src/components/pagination.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = props => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+
+  const pageCount = Math.ceil(itemsCount / pageSize);
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            className={(page === currentPage) ? "page-item active" : "page-item"}
+            key={page}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
