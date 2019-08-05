1ad8312234a688c3b4e90b386bb8a1b655b371f1
msaaddev <mrsaadirfan@gmail.com>
2019-08-05 18:38:18 +0500

    📦NEW: pagination method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./like";
 import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
@@ -29,9 +30,10 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const {pageSize, currentPage} = this.state;
+    const {pageSize, currentPage, movies: allMovies} = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
+    const movies = paginate(allMovies, currentPage, pageSize)
 
     return (
       <React.Fragment>
@@ -47,7 +49,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
