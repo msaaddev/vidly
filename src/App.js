5e5f2415622656a4cda7185c62fea75fd88fec68
msaaddev <mrsaadirfan@gmail.com>
2019-08-18 17:31:27 +0500

    📦NEW: not found component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import Movies from "../src/components/movies";
-import Nav from "../src/components/navigation";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import Movies from "../src/components/movies";
+import NavBar from "../src/components/navBar";
+import Customers from "../src/components/customers";
+import Rentals from "../src/components/rentals";
+import NotFound from '../src/components/notFound';
+
 
 function App() {
   return (
     <React.Fragment>
-      <Nav />
+      <NavBar />
+      <br />
+      <Switch>
+        <Route path="/customers" component={Customers} />
+        <Route path="/rental" component={Rentals} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/not-found" component={NotFound}/>
+        <Route path="/" exact component={Movies} />
+        <Redirect to="/not-found" />
+      </Switch>
     </React.Fragment>
   );
 }
