47c94de6b7500412b8683aec2f4ad17f58f9c8ed
msaaddev <mrsaadirfan@gmail.com>
2019-08-20 18:30:24 +0500

    📦NEW: Joi library

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./input";
 
 class LoginForm extends Component {
@@ -7,7 +8,17 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
