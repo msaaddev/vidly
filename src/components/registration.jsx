884bea601ba2319e26e8079ece6cd2622313f5c5
msaaddev <mrsaadirfan@gmail.com>
2019-08-22 17:07:25 +0500

    📦NEW: form

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 
 class RegistrationForm extends Component {
   state = {};
   render() {
-    return <h1>Registration</h1>;
+    return (
+      <div>
+        <h1>Register</h1>
+        <form action="">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input id="name" type="text" className="form-control" />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Register
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
