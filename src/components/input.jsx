115c2e8aefedff87221c32b900df472a3ad0bf92
msaaddev <mrsaadirfan@gmail.com>
2019-08-22 16:25:26 +0500

    📦NEW: rest operator

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, error, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        id={name}
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
