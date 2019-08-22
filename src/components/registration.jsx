f2db9b28049322c17cd419f47d92f89b3b87ea28
msaaddev <mrsaadirfan@gmail.com>
2019-08-22 17:23:15 +0500

    📦NEW: handleSubmit, state, handleChange

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,26 +1,59 @@
 import React, { Component } from "react";
-import Joi from "joi-browser";
 
 class RegistrationForm extends Component {
-  state = {};
+  state = {
+    data: { username: "", password: "", nameOfUser: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    const data = { ...this.state.data };
+    data[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ data    });
+  };
+
   render() {
     return (
       <div>
         <h1>Register</h1>
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              name="username"
+              value={this.state.data.username}
+              id="username"
+              type="text"
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              name="password"
+              value={this.state.data.password}
+              id="password"
+              type="text"
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input id="name" type="text" className="form-control" />
+            <input
+              name="nameOfUser"
+              value={this.state.data.nameOfUser}
+              id="name"
+              type="text"
+              onChange={this.handleChange}
+              className="form-control"
+            />
           </div>
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Register
           </button>
         </form>
