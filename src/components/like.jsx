994c477b16fb020f43d215e596fed654af1e9c82
msaaddev <mrsaadirfan@gmail.com>
2019-08-23 16:55:34 +0500

    📦NEW: MovieForm class

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Like = (props) => {
-    let classes = "fa fa-heart";
-    if (!props.liked) classes += "-o";
-    return (
-      <i
-        className={classes}
-        style={{ cursor: "pointer" }}
-        onClick = {props.onClick}
-        aria-hidden="true"
-      />
-    );
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      className={classes}
+      style={{ cursor: "pointer" }}
+      onClick={props.onClick}
+      aria-hidden="true"
+    />
+  );
 };
 
 export default Like;
--- src/components/newMovie.jsx
+++ src/components/newMovie.jsx
@@ -1,10 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NewMovie extends Component {
-    state = {  }
-    render() {
-        return <h1>New Movie</h1>
-    }
+  state = {
+    data: { title: "", genre: "", noOfStock: "", rate: "" }
+  };
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            value={this.state.data.title}
+            id="title"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="genre">Genre</label>
+          <select
+            name="genre"
+            value={this.state.data.genre}
+            id="genre"
+            type="text"
+            className="form-control"
+          >
+            <option />
+            <option>Action</option>
+            <option>Comedy</option>
+            <option>Thriller</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="noOfStock">Number in Stock</label>
+          <input
+            name="noOfStock"
+            value={this.state.data.noOfStock}
+            id="noOfStock"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rate">Rate</label>
+          <input
+            name="rate"
+            value={this.state.data.rate}
+            id="rate"
+            type="text"
+            className="form-control"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NewMovie;
