2fddb7be643cfd8d71a80e53d17a21825d65e515
msaaddev <mrsaadirfan@gmail.com>
2019-08-30 19:42:28 +0500

    📦NEW: httpServices.js

---
+++ src/services/httpService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status <= 500;
+
+  if (!expectedError) {
+    console.log("Logging the error", error);
+    toast.error("An unexpected error occurred");
+  }
+
+  return Promise.reject(error);
+});
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+};
