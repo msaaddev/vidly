3b19a1c09e28f66573080ab29d5d7f1c8583fa04
msaaddev <mrsaadirfan@gmail.com>
2019-08-30 19:47:18 +0500

    📦NEW: logService.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Movies from "../src/components/movies";
 import NavBar from "../src/components/navBar";
 import Customers from "../src/components/customers";
 import Rentals from "../src/components/rentals";
+import { ToastContainer } from "react-toastify";
 import NotFound from "../src/components/notFound";
 import LoginForm from "./components/loginform";
-import EditMovie from './components/movieForm';
+import EditMovie from "./components/movieForm";
 import RegistrationForm from "./components/registration";
 import NewMovie from "./components/newMovie";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
+      <ToastContainer />
       <main className="container">
         <Switch>
           <Route path="/movies/new" component={NewMovie} />
