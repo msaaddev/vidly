8436a43e28ecd207e6ff9abd6bf3104367a735bf
msaaddev <mrsaadirfan@gmail.com>
2019-09-01 00:17:38 +0500

    📦NEW: userService.js

--- src/components/newMovieForm.jsx
+++ src/components/newMovieForm.jsx
@@ -12,6 +12,7 @@ class NewMovieForm extends Component {
     e.preventDefault();
     const errors = this.validate();
     this.setState({ errors: errors || {} });
+    if (errors) return;
 
     this.doSubmit();
   };
--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,46 +1,47 @@
-  import React from "react";
-  import Joi from "joi-browser";
-  import Form from "./common/form";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import * as userService from "../services/userService";
 
-  class RegistrationForm extends Form {
-    state = {
-      data: { username: "", password: "", nameOfUser: "" },
-      errors: {}
-    };
+class RegistrationForm extends Form {
+  state = {
+    data: { username: "", password: "", nameOfUser: "" },
+    errors: {}
+  };
 
-    schema = {
-      username: Joi.string()
-        .email()
-        .required()
-        .label("Username"),
-      password: Joi.string()
-        .alphanum()
-        .min(5)
-        .required()
-        .label("Password"),
-      nameOfUser: Joi.string()
-        .label("Name")
-        .required()
-    };
+  schema = {
+    username: Joi.string()
+      .email()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .alphanum()
+      .min(5)
+      .required()
+      .label("Password"),
+    nameOfUser: Joi.string()
+      .label("Name")
+      .required()
+  };
 
-    doSubmit = () => {
-      // Call the server
-      console.log("Submitted");
-    };
+  doSubmit = async () => {
+    // Call the server
+    await userService.register(this.state.data)
+  };
 
-    render() {
-      return (
-        <div>
-          <h1>Registration</h1>
-          <form onSubmit={this.handleSubmit}>
-            {this.renderInput("username", "Username")}
-            {this.renderInput("password", "Password", "password")}
-            {this.renderInput("nameOfUser", "Name")}
-            {this.renderButton("Register")}
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Registration</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("nameOfUser", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
   }
+}
 
-  export default RegistrationForm;
+export default RegistrationForm;
