4c4b1c7b2c6c7184d2ea48759dd70e75083d4e60
msaaddev <mrsaadirfan@gmail.com>
2019-09-05 17:54:52 +0500

    📦NEW: environment variables

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+console.log(process.env)
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
